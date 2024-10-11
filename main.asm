7549888b5f75a1d081e1bd9623157d53b9039db0
Yann Herren <hycmedia@icloud.com>
2024-10-11 23:43:04 +0200

    enhance template

--- main.asm
+++ main.asm
@@ -1,12 +1,17 @@
+;   syscall table: https://x64.syscall.sh/
+
 SECTION .data
+    ; define data here
+
+SECTION .bss
+    ; reserve memory here
 
 SECTION .text
-
 global _start
 
 _start:
     ; your code goes here
 
-    mov rax, 60
-    mov rdi, 0
+    mov rax, 60 ; syscall for exit
+    mov rdi, 0 ; exit code 0
     syscall
