f5e6b62289b3b65000c7943142509f7b5401696b
Herren Yann Cédric <yann.herren@students.bfh.ch>
2024-11-04 20:48:15 +0100

    implement simple hex dump program

--- main.asm
+++ main.asm
@@ -1,17 +1,68 @@
 ;   syscall table: https://x64.syscall.sh/
 
 SECTION .data
-    ; define data here
+    HexTable: db "0123456789ABCDEF", 0x20, 0x0A
 
 SECTION .bss
-    ; reserve memory here
+    Buffer: resb 8
+    BufferLen equ $ - Buffer
 
 SECTION .text
 global _start
 
 _start:
-    ; your code goes here
 
+read:
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, Buffer
+    mov rdx, BufferLen
+    syscall
+
+    cmp rax, 0
+    jz exit
+
+    xor r10, r10
+
+process:
+    cmp r10, rax
+    jz read_next
+    mov byte bl, [Buffer+r10]
+
+    movzx rbx, bl
+    push rbx
+
+    shr rbx, 4
+    call write_char
+
+    pop rbx
+    and rbx, 0x0F
+    call write_char
+
+    mov rbx, 0x10
+    call write_char
+
+    inc r10
+
+    jmp process
+
+write_char:
+    push rax
+    lea r8, [HexTable+rbx]
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, r8
+    mov rdx, 1
+    syscall
+    pop rax
+    ret
+
+read_next:
+    mov rbx, 0x11
+    call write_char
+    jmp read
+
+exit:
     mov rax, 60 ; syscall for exit
     mov rdi, 0 ; exit code 0
     syscall
